c8e06f250c1291f5e274c348889761904cd8a86e
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-24 09:48:45 +0200

    on scroll change color

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 window.addEventListener('scroll', function () {
+
     let header = document.querySelector('header');
+
+    let social = document.querySelector('.social')
+
+    let windowsPosition = window.scrollY > 40;
+
     let windowPosition = window.scrollY > 800;
+
     header.classList.toggle('scrolling-active', windowPosition);
+
+    social.classList.toggle('svg-active', windowsPosition);
 })
+
