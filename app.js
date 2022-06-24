0a17099376e61c0c0191ca7dcbb04b8ca2496016
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-24 08:06:44 +0200

    change color on scroll

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-jz
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('header');
+    let windowPosition = window.scrollY > 800;
+    header.classList.toggle('scrolling-active', windowPosition);
+})
