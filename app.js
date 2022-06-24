2b787106c3cc3eee412fa8c6736e9f36e78269fe
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-24 11:42:44 +0200

    plane move

--- app.js
+++ app.js
@@ -13,3 +13,19 @@ window.addEventListener('scroll', function () {
     social.classList.toggle('svg-active', windowsPosition);
 })
 
+
+
+const SUBMIT = document.querySelector('#submit');
+
+const submitForm = (e) => {
+    if (!e.target.classList.contains('active')) {
+        e.target.classList.add('active');
+        setTimeout(() => {
+            e.target.classList.remove('active');
+            e.target.classList.add('done');
+            e.target.innerHTML = 'Done'
+        }, 1000);
+    }
+}
+
+SUBMIT.addEventListener('click', submitForm);
