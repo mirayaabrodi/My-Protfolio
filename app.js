bf3802f542772f58bed37bc8608c7ad06c279143
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-02 10:49:25 +0200

    format document

--- app.js
+++ app.js
@@ -5,26 +5,30 @@ window.addEventListener('scroll', function () {
     let navPosition = window.scrollY > 750;
     let svgPosition = window.scrollY > 40;
 
-
-    let navContactPosition = window.scrollY > 4800;
-    let svgContactPosition = window.scrollY > 3900;
-
-    let mobilePositionContact = window.scrollY > 4850;
-    let mobilePositionContactSvg = window.scrollY > 4850;
-
-
-
-
     header.classList.toggle('scrolling-active', navPosition);
     social.classList.toggle('svg-active', svgPosition);
 
+
+    let navContactPosition = window.scrollY > 5200;
+    let svgContactPosition = window.scrollY > 4400;
 
     header.classList.toggle('contact-active', navContactPosition)
     social.classList.toggle('contact-svg', svgContactPosition)
 
 
+
+    let mobilePositionContact = window.scrollY > 4850;
+    let mobilePositionContactSvg = window.scrollY > 4850;
     header.classList.toggle('mobile-contact-active', mobilePositionContact)
     social.classList.toggle('mobile-contact-svg', mobilePositionContactSvg)
+
+
+    let mobileNav = window.scrollY > 600;
+    header.classList.toggle('six-mobile-width', mobileNav);
+
+
+
+
 })
 
 // Submit contact button
